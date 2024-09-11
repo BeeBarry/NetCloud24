0098e4ca00c2d62da53a587a4f0fa8bf7c48d2eb
BeeBarry <barry.namdari@gmail.com>
2024-09-11 20:43:38 +0200

    Create TestRunner

---
+++ Exercises/OOP Exercises /OOP Exercises /TestRunner.cs
@@ -0,0 +1,32 @@
+namespace OOP_Exercises;
+
+public class TestRunner
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Call the test method for the program you want to tes..");
+        Console.WriteLine();
+    }
+
+
+    public static void TestLibrarySystem()
+    {
+        Console.WriteLine("Test library system:");
+        Console.WriteLine("=====================");
+
+        var library = new Program.Library();
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
