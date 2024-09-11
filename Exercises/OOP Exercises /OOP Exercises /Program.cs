ad3eb88da5b32f223eda71359bb9ff2e2e9db1d0
BeeBarry <barry.namdari@gmail.com>
2024-09-11 19:12:13 +0200

    Update Program.cs

--- Exercises/OOP Exercises /OOP Exercises /Program.cs
+++ Exercises/OOP Exercises /OOP Exercises /Program.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// This is a library system program that manages books, loanees and loans in a library.
 
-Console.WriteLine("Hello, World!");
+public class Library
+{
+    private List<Book> Books { get; } = new List<Book>();
+    private List<LibraryUser> Users { get; } = new List<LibraryUser>();
+
+
+
+    public void AddBook(Book book)
+    {
+        Books.Add(book);
+    }
+
+    public void AddUser(LibraryUser user)
+    {
+        Users.Add(user);
+    }
+
+    public void DisplayInformation()
+    {
+        Console.WriteLine("Books available");
+        foreach (var book in Books)
+        {
+            Console.WriteLine($"- {book.Title} by {book.Author} (ISBN: {book.ISBN})");
+        }
+
+        Console.WriteLine("\nLibrary Users:");
+        foreach (var user in Users)
+        {
+            Console.WriteLine($"- {user.Name} (ID: {user.UserId})");
+        }
+        
+    }
+
+
+}
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+
+    public class LibraryUser
+    {
+        public string Name { get; set; }
+        public string UserId { get; set; }
+    }
+
+    public class Loan
+    {
+        public Book LoanedBook { get; set; }
+        public LibraryUser Loanee { get; set; }
+    }
+
+
